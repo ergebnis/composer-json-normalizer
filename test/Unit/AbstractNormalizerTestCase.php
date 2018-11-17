44730c43eb703c9512d3b92ff7d45c0936d66ff9
Andreas Möller <am@localheinz.com>
2018-11-17 15:17:32 +0100

    Enhancement: Import normalizers from localheinz/composer-normalize

---
+++ test/Unit/AbstractNormalizerTestCase.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2018 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/composer-json-normalizer
+ */
+
+namespace Localheinz\Composer\Json\Normalizer\Test\Unit;
+
+use Localheinz\Json\Normalizer\Json;
+use Localheinz\Json\Normalizer\NormalizerInterface;
+use Localheinz\Test\Util\Helper;
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ */
+abstract class AbstractNormalizerTestCase extends Framework\TestCase
+{
+    use Helper;
+
+    final public function testImplementsNormalizerInterface(): void
+    {
+        $this->assertClassImplementsInterface(NormalizerInterface::class, $this->className());
+    }
+
+    /**
+     * @dataProvider providerJsonNotDecodingToObject
+     *
+     * @param string $encoded
+     */
+    final public function testNormalizeDoesNotModifyWhenJsonDecodedIsNotAnObject(string $encoded): void
+    {
+        $json = Json::fromEncoded($encoded);
+
+        $reflection = new \ReflectionClass($this->className());
+
+        /** @var NormalizerInterface $normalizer */
+        $normalizer = $reflection->newInstanceWithoutConstructor();
+
+        $normalized = $normalizer->normalize($json);
+
+        $this->assertSame($json->encoded(), $normalized->encoded());
+    }
+
+    public function providerJsonNotDecodingToObject(): \Generator
+    {
+        $faker = $this->faker();
+
+        $values = [
+            'array' => $faker->words,
+            'bool-false' => false,
+            'bool-true' => true,
+            'float' => $faker->randomFloat(),
+            'int' => $faker->randomNumber(),
+            'null' => null,
+            'string' => $faker->sentence,
+        ];
+
+        foreach ($values as $key => $value) {
+            yield $key => [
+                \json_encode($value),
+            ];
+        }
+    }
+
+    final protected function className(): string
+    {
+        return \preg_replace(
+            '/Test$/',
+            '',
+            \str_replace(
+                'Localheinz\\Composer\\Json\\Normalizer\\Test\\Unit\\',
+                'Localheinz\\Composer\\Json\\Normalizer\\',
+                static::class
+            )
+        );
+    }
+}
