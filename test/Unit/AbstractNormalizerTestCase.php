e093d4e1d8a8e33a842b2bf48d7ac941af91a781
Andreas Möller <am@localheinz.com>
2019-12-07 10:00:02 +0100

    Enhancement: Use ergebnis/test-util instead of localheinz/test-util

--- test/Unit/AbstractNormalizerTestCase.php
+++ test/Unit/AbstractNormalizerTestCase.php
@@ -13,9 +13,9 @@ declare(strict_types=1);
 
 namespace Localheinz\Composer\Json\Normalizer\Test\Unit;
 
+use Ergebnis\Test\Util\Helper;
 use Localheinz\Json\Normalizer\Json;
 use Localheinz\Json\Normalizer\NormalizerInterface;
-use Localheinz\Test\Util\Helper;
 use PHPUnit\Framework;
 
 /**
